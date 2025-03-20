d888dcef291ce3aabad15ba2ab2f8e56cb4ce872
Kevin M <Kevinx7276@gmail.com>
2025-03-20 18:50:17 -0500

    Cambios otra ves 2.0

--- crear-categoria.php
+++ crear-categoria.php
@@ -18,7 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre_categoria'])) {
             $stmt->bind_param("si", $nombre_categoria, $id_categoria);
 
             if ($stmt->execute()) {
-                $mensaje = "<button class='btn success'>Categoría actualizada exitosamente</button>";
+                header("Location: crear-categoria.php"); 
+                exit();
             } else {
                 $mensaje = "<button class='btn error'>Error al actualizar la categoría</button>";
             }
@@ -56,7 +57,8 @@ if (isset($_GET['eliminar'])) {
         $stmt->bind_param("i", $id_categoria);
         
         if ($stmt->execute()) {
-            $mensaje = "<button class='btn success'>Categoría eliminada correctamente</button>";
+            header("Location: crear-categoria.php"); 
+                exit();
         } else {
             $mensaje = "<button class='btn error'>Error al eliminar la categoría</button>";
         }
